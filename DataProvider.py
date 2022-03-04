9b945cb7059b2fb7eb5e91619d38dfb8e3c3b129
ignitedevua <ignitedevua@gmail.com>
2022-03-04 20:38:47 +0200

    update

--- DataProvider.py
+++ DataProvider.py
@@ -13,7 +13,7 @@ from urllib3 import disable_warnings
 
 from urllib.parse import urlparse
 
-HOSTS = 'https://raw.githubusercontent.com/opengs/uashieldtargets/master/sites.json'
+HOSTS = 'https://github.com/opengs/uashieldtargets/blob/v2/sites.json'
 PROXIES_URL = 'https://raw.githubusercontent.com/opengs/uashieldtargets/master/proxy.json'
 
 
@@ -22,6 +22,7 @@ class DataProvider:
         self._proxies = []
         self._sites = []
         self.index = 0
+        self._lastupdate = 0
         # self.eTag = ''
 
     def get_url(self):
@@ -47,9 +48,10 @@ class DataProvider:
 
 
     def get_sites(self, url: str = HOSTS):
-        if not self._sites or self.index <= 0:
+        if not self._sites or time.time() > self._lastupdate + 60*60: #updte every hour
             while True:
                 try:
+                    # print("updating targets...")
                     # https://api.github.com/repos/opengs/uashieldtargets/commits?path=sites.json&page=1&per_page=1
                     # response = requests.head(url, allow_redirects=False)
                     # if response.status_code != 200:
@@ -67,11 +69,11 @@ class DataProvider:
                         if 'attack' not in site or ('attack' in site and not site['attack'] == 0):
                             domain = urlparse(unquote(site['page']))
                             self._sites.append(domain.netloc)
-                    self.index = len(self._sites)
+                    self._lastupdate = time.time()
                     break
                 except:
                     continue
-
+        self.index = len(self._sites)
         return self._sites
     
     def fix_url(self, url: str, force_https: bool = False) -> str:
